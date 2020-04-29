58955ac5c3176341500f51741c86c81911cccb90
chrisshank <chris.shank.23@gmail.com>
2020-04-28 21:21:40 -0400

    update depenedecies for Vue and Vuex Beta

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -3,7 +3,3 @@ declare module '*.vue' {
 	const component: ComponentOptions
 	export default component
 }
-
-// Vuex 4 Alpha 1 does not have typings yet...
-// Remove this when typings are added to Vuex 4 Alpha 
-declare module 'vuex'
--- src/store.ts
+++ src/store.ts
@@ -1,7 +1,6 @@
 import { createStore } from 'vuex'
 
-// Temorary type until Vuex 4 Alpha has Typescript support
-type State = { count: number }
+export type State = { count: number }
 
 const state: State = { count: 0 }
 
